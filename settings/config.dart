ae03f80609a50feab2c1d5bce7afe8a60b36bf2e
Ibrahim Kalebasi <73583278+kalebasi@users.noreply.github.com>
2022-11-29 22:55:37 +0300

    Delete config.dart

--- settings/config.dart
+++
@@ -1,7 +0,0 @@
-
-class Config {
-  static const url="10.0.2.2:10000"; //Emülatörden çalışmak için IIS Express
- // static const url="10.0.2.2:3131"; //Emülatörden çalışmak için  WEBAPI
-  //  static const url="192.168.1.45:8031"; //Telefondan çalışmak için  IIS
-  //  static const url="192.168.1.45:10000"; //Telefondan çalışmak için  IIS
-}
